e67b8016d1dd17c1d441e4d60b9cb252619a61d8
Zahid <zahid67635gmail.com>
2022-11-08 02:08:41 +0600

    login signup added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+    }
     return (
-        <div>
-            login
+        <div className="w-full my-12">
+            <div className="grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+
+                    {/* lottieeeeee */}
+
+                </div>
+                <div className="card flex-shrink-0 md:w-3/4 shadow-2xl bg-base-100 py-16">
+                    <h1 className="text-5xl text-center font-bold">Sign In</h1>
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <p className='text-center'>New to DocPort? <Link className='text-orange-600 font-bold' to="/register">Sign Up</Link> </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const handleRegister = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+    }
     return (
-        <div>
-            Register
+        <div className="card md:w-1/2 shadow-2xl bg-base-100 py-16 mx-auto">
+            <h1 className="text-5xl text-center font-bold">Sign Up</h1>
+            <form onSubmit={handleRegister} className="card-body">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input type="text" name='email' placeholder="Name" className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" name='password' placeholder="password" className="input input-bordered" />
+
+                </div>
+                <div className="form-control mt-6">
+                    <input className="btn btn-primary" type="submit" value="Register" />
+                </div>
+            </form>
+            <p className='text-center'>Already have an account? <Link className='text-orange-600 font-bold' to="/login">Sign In</Link> </p>
         </div>
     );
 };
